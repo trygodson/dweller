b530718e0ed72872b4ed7c04d8936e38ab92e079
trygodson <gnqnpn@gmail.com>
2022-07-08 07:49:21 +0100

    image adjustment

--- src/Entryfile/imagepath.jsx
+++ src/Entryfile/imagepath.jsx
@@ -7,6 +7,6 @@
 export { default as bgAuthImage } from '../assets/images/bgd1.png';
 export { default as authOverlay } from '../assets/images/registerOverlay.png';
 export { default as logo } from '../assets/images/dweller-logo.png';
-export { default as logowhite } from '../assets/images/icon white.png';
+export { default as logowhite } from '../assets/images/dweller-white.png';
 export { default as collage1 } from '../assets/images/collage-1.jpg';
 export { default as collage2 } from '../assets/images/collage-2.jpg';
