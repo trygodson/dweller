7fefb776dad2f98ac233d73f6dfaf6b3ee594c66
trygodson <gnqnpn@gmail.com>
2022-07-13 02:03:48 +0100

    removed images

--- src/Entryfile/imagepath.jsx
+++ src/Entryfile/imagepath.jsx
@@ -4,9 +4,7 @@
 
 // export default imagepaths
 
-export { default as bgAuthImage } from '../assets/images/bgd1.png';
 export { default as authOverlay } from '../assets/images/registerOverlay.png';
 export { default as logo } from '../assets/images/dweller-logo.png';
 export { default as logowhite } from '../assets/images/dweller-white.png';
 export { default as emptyicon } from '../assets/empty.png';
-export { default as collage2 } from '../assets/images/collage-2.jpg';
